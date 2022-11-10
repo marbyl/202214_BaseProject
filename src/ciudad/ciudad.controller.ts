f24e8baa584fcefaefcc01857f9a7f5c943cab28
Marby Leguizamon <marbyptt@gmail.com>
2022-11-09 21:44:33 -0500

    aldskfj

--- src/ciudad/ciudad.controller.ts
+++ src/ciudad/ciudad.controller.ts
@@ -6,15 +6,6 @@ import { CiudadEntity } from './ciudad.entity';
 import { CiudadService } from './ciudad.service';
 
 @Controller('ciudad')
-// axc
-// axc
-// axc
-// axc
-// axc
-// axc
-// axc
-// axc
-// axc
 @UseInterceptors(BusinessErrorsInterceptor)
 export class CiudadController {
     constructor(private readonly ciudadService: CiudadService) {}
