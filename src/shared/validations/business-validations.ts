2cdc7ec25fd597c1edafd914da814f59eeeda9a3
coandrade <co.andrade86@uniandes.edu.co>
2022-11-09 22:44:09 -0500

    Ajustes Code smell src/shared/validations/business-validations.ts

--- src/shared/validations/business-validations.ts
+++ src/shared/validations/business-validations.ts
@@ -1,12 +1,12 @@
 export function esPaisPermitido(pais: string): boolean{
-    var paisesPermitidos : string[] = ['Argentina', 'Ecuador', 'Paraguay'];
+    let paisesPermitidos : string[] = ['Argentina', 'Ecuador', 'Paraguay'];
     if (paisesPermitidos.includes(pais))
         return true;
     return false;
 }
 
 export function tieneLongitudRequerida(nombreSupermercado: string): boolean{
-    var longitudNombreSupermercado : number = nombreSupermercado.length;
+    let longitudNombreSupermercado : number = nombreSupermercado.length;
     if (longitudNombreSupermercado > 10)
         return true;
     return false;
